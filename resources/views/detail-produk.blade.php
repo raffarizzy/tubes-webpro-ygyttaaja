27867ef8ded798d06ff96e3ce8b29ca9cb577544
naufa <naufal.mdzulfik@gmail.com>
2025-12-25 15:32:20 +0700

    menambahkan API untuk detail-produk lalu mengubah sedikit jsnya

--- resources/views/detail-produk.blade.php
+++ resources/views/detail-produk.blade.php
@@ -2,15 +2,10 @@
 
 @section('title', 'Detail Produk - SpareHub')
 
-@section('body-class', 'class="bg-light"')
-
-@push('bootstrap')
+@push('styles')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 @endpush
-
-@section('footer-class', 'class="text-center bg-primary text-white py-3"')
-@section('footer-text-class', 'class="m-0"')
 
 @section('content')
   <!-- KONTEN -->
